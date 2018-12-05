8e2c905686926a21e5a8b4ed45f275eb28f3c51d
PeiChun Tseng <44983115+PeichunTseng@users.noreply.github.com>
2018-12-04 20:33:30 -0500

    UpdateController

--- src/neu/edu/info6205/team/controller/Controller.java
+++ src/neu/edu/info6205/team/controller/Controller.java
@@ -1,8 +1,38 @@
 package neu.edu.info6205.team.controller;
 
 public class Controller {
-	public static void demo() {
-		// TODO
-		// Maze generalize, GA flow
-	}
+    public static void demo() {
+	public static int maxGenerations = 1000;
+    Maze m = new Maze(new int[][] { 
+      { 0, 0, 0, 0, 1, 0, 1, 3, 2 }, 
+      { 1, 0, 1, 1, 1, 0, 1, 3, 1 },
+      { 1, 0, 0, 1, 3, 3, 3, 3, 1 }, 
+      { 3, 3, 3, 1, 3, 1, 1, 0, 1 }, 
+      { 3, 1, 3, 3, 3, 1, 1, 0, 0 },
+      { 3, 3, 1, 1, 1, 1, 0, 1, 1 }, 
+      { 1, 3, 0, 1, 3, 3, 3, 3, 3 }, 
+      { 0, 3, 1, 1, 3, 1, 0, 1, 3 },
+      { 1, 3, 3, 3, 3, 1, 1, 1, 4 } 
+                                  });
+    GeneticAlgorithm gna = new GeneticAlgorithm(200, 0.05, 0.9, 2, 10);
+    Population pop = gna.initPopulation(128);
+    gna.evalPopulation(pop, m);
+    int generation = 1;
+
+    while (gna.isTerminationConditionMet(generation, maxGenerations) == false) 
+    {
+      Individual fittest = pop.getFittest(0);
+      System.out.println("G" + generation + " Best solution (" + fittest.getFitness() + "): " + fittest.toString());
+      pop = gna.crossoverPopulation(pop);
+      pop = gna.mutatePopulation(pop);
+      gna.evalPopulation(pop, m);
+      generation++;
+    }
+
+    System.out.println("Stopped after " + maxGenerations + " generations.");
+    Individual fittest = pop.getFittest(0);
+    System.out.println("Best solution (" + fittest.getFitness() + "): " + fittest.toString());
+
+		
+    }
 }

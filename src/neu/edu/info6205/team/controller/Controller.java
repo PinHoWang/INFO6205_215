5347c0c0bef2552cae10760b33f9956a2d9f2ea7
PeiChun Tseng <44983115+PeichunTseng@users.noreply.github.com>
2018-12-04 20:52:29 -0500

    change function name

--- src/neu/edu/info6205/team/controller/Controller.java
+++ src/neu/edu/info6205/team/controller/Controller.java
@@ -1,8 +1,8 @@
 package neu.edu.info6205.team.controller;
 
 public class Controller {
-    public static void demo() {
-	public static int maxGenerations = 1000;
+ public static int maxGenerations = 1000;
+  public static void demo() {
     Maze m = new Maze(new int[][] { 
       { 0, 0, 0, 0, 1, 0, 1, 3, 2 }, 
       { 1, 0, 1, 1, 1, 0, 1, 3, 1 },
@@ -15,17 +15,17 @@ public class Controller {
       { 1, 3, 3, 3, 3, 1, 1, 1, 4 } 
                                   });
     GeneticAlgorithm gna = new GeneticAlgorithm(200, 0.05, 0.9, 2, 10);
-    Population pop = gna.initPopulation(128);
-    gna.evalPopulation(pop, m);
+    Population pop = gna.firstPopulation(128);
+    gna.calcPopulation(pop, m);
     int generation = 1;
 
-    while (gna.isTerminationConditionMet(generation, maxGenerations) == false) 
+    while (gna.endConditionMet(generation, maxGenerations) == false) 
     {
       Individual fittest = pop.getFittest(0);
       System.out.println("G" + generation + " Best solution (" + fittest.getFitness() + "): " + fittest.toString());
-      pop = gna.crossoverPopulation(pop);
-      pop = gna.mutatePopulation(pop);
-      gna.evalPopulation(pop, m);
+      pop = gna.crossover(pop);
+      pop = gna.mutate(pop);
+      gna.calcPopulation(pop, m);
       generation++;
     }
 
@@ -33,6 +33,6 @@ public class Controller {
     Individual fittest = pop.getFittest(0);
     System.out.println("Best solution (" + fittest.getFitness() + "): " + fittest.toString());
 
-		
+    
     }
 }
